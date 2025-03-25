61da8354fc22215e95dcb3ca34e631d305801e60
Ivan Vorobev <ivan.vorobjov@gmail.com>
2025-03-25 22:11:39 +0200

    Final commit

--- app/routes.py
+++ app/routes.py
@@ -2,9 +2,9 @@ from flask import render_template, redirect, url_for, request
 from app import app
 
 posts = []
-
+userinfo = {}
 @app.route('/')
-def index():
+def mainpage():
     return render_template('index.html')
 
 @app.route('/blog/', methods=['GET', 'POST'])
@@ -16,3 +16,19 @@ def blog():
             posts.append({'title': title, 'text': text})
         return redirect(url_for('blog'))
     return render_template('blog.html', posts=posts)
+
+@app.route('/user/', methods=['GET', 'POST'])
+def user():
+    if request.method == 'POST':
+        userinfo["name"] = request.form['name']
+        userinfo["address"] = request.form['address']
+        userinfo["hobby"] = request.form['hobby']
+        userinfo["age"] = request.form['age']
+        return redirect(url_for('user'))
+    return render_template('user.html', user=userinfo)
+
+@app.route('/user/edit')
+def user_edit():
+    if userinfo=={}:
+        return redirect(url_for('user'))
+    return render_template('user_edit.html', user=userinfo)
